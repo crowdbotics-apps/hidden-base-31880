89419515d45132ebd953d7b3d10717290ad09c14
Crowdbotics <team@crowdbotics.com>
2021-11-08 20:24:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hidden_base_31880",
+  url: "https://hidden_base_31880.botics.co",
+  api: "https://hidden_base_31880.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
